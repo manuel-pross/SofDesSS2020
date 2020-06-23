54d7a61cf55f35ffa4217e40e75fdc4ff385ea6c
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-06-23 17:36:05 +0200

    TASK: add iterator

--- L3_Generischer_Baum/Main.js
+++ L3_Generischer_Baum/Main.js
@@ -1,8 +1,11 @@
 "use strict";
 class TreeNode {
-    constructor(_name) {
+    constructor(_value) {
         this.childs = [];
-        this.name = _name;
+        this.value = _value;
+    }
+    toString() {
+        return this.value + "";
     }
     appendChild(_newChild) {
         _newChild.parent = this;
@@ -14,7 +17,7 @@ class TreeNode {
                 this.childs.splice(i, 1);
     }
     getTreeAsString(_depth = 0) {
-        let treeAsString = this.name + "\n";
+        let treeAsString = this.value + "\n";
         let depthAsString = "";
         for (let i = 0; i < _depth; i++)
             depthAsString += "*";
@@ -28,15 +31,52 @@ class TreeNode {
     printTree() {
         console.log(this.getTreeAsString());
     }
+    search(_pattern, _result = []) {
+        if (_pattern(this.value))
+            _result.push(this);
+        for (let i = 0; i < this.childs.length; i++)
+            this.childs[i].search(_pattern, _result);
+        return _result;
+    }
 }
 class Tree {
+    constructor() {
+        this.nodes = [];
+        this.observers = [];
+    }
+    registerObserver(_observer) {
+        this.observers.push(_observer);
+    }
+    removeObserver(_observer) {
+        this.observers.splice(this.observers.indexOf(_observer), 1);
+    }
+    notifyObservers() {
+        for (let observer of this.observers)
+            observer.update(this.nodes.length);
+    }
     createNode(_newNode) {
         let newNode = new TreeNode(_newNode);
+        this.nodes.push(newNode);
+        this.notifyObservers();
         return newNode;
+    }
+}
+class TreeObserver {
+    update(_countNodes) {
+        console.log("Anzahl der Knoten: " + _countNodes);
+    }
+}
+function* iAmAGenerator(_tree) {
+    for (let i = 0; i < _tree.nodes.length; i++) {
+        const inside = _tree.nodes[i].toString();
+        yield inside;
     }
 }
 let tree = new Tree();
 let root = tree.createNode("root");
+const iterator = iAmAGenerator(tree);
+let observer1 = new TreeObserver();
+tree.registerObserver(observer1);
 let child1 = tree.createNode("child1");
 let child2 = tree.createNode("child2");
 let grand11 = tree.createNode("grand11");
@@ -53,5 +93,7 @@ child2.appendChild(grand21);
 root.appendChild(child2);
 grand21.appendChild(grand212);
 child2.appendChild(grand22);
-root.printTree();
+console.log(iterator.next());
+// root.printTree();
+// console.log(root.search(x => x.includes("212")));
 //# sourceMappingURL=Main.js.map
--- L3_Generischer_Baum/Main.ts
+++ L3_Generischer_Baum/Main.ts
@@ -1,10 +1,27 @@
+interface Subject {
+    registerObserver(_observer: Observer): void;
+
+    removeObserver(_observer: Observer): void;
+
+    notifyObservers(): void;
+}
+
+interface Observer {
+    update(_countNodes: number): void;
+}
+
+
 class TreeNode<T> {
-    public name: T;
+    public value: T;
     public childs: TreeNode<T>[] = [];
     public parent: TreeNode<T>;
 
-    constructor(_name: T) {
-        this.name = _name;
+    constructor(_value: T) {
+        this.value = _value;
+    }
+
+    public toString(): String {
+        return this.value + "";
     }
 
     public appendChild(_newChild: TreeNode<T>): void {
@@ -19,7 +36,7 @@ class TreeNode<T> {
     }
 
     public getTreeAsString(_depth: number = 0): String {
-        let treeAsString: String = this.name + "\n";
+        let treeAsString: String = this.value + "\n";
         let depthAsString: String = "";
 
         for (let i: number = 0; i < _depth; i++)
@@ -39,31 +56,63 @@ class TreeNode<T> {
         console.log(this.getTreeAsString());
     }
 
-    // public search(_pattern: T): TreeNode<T> {
-    //     let result: TreeNode<T> = null;
+    public search(_pattern: (_value: T) => boolean, _result: TreeNode<T>[] = []): TreeNode<T>[] {
 
-    //     if (this.name == _pattern) {
-    //         result = this;
-    //         return result;
-    //     }
-    //     else {
-    //         for (let i: number = 0; i < this.childs.length; i++) {
-
-    //         }
-    //     }
-    // }
+        if (_pattern(this.value))
+            _result.push(this);
+        for (let i: number = 0; i < this.childs.length; i++)
+            this.childs[i].search(_pattern, _result);
+        return _result;
+    }
 }
 
-class Tree<T> {
+class Tree<T> implements Subject{
+    public nodes: TreeNode<T>[] = [];
+    public observers: Observer[] = [];
+
+    registerObserver(_observer: Observer): void {
+        this.observers.push(_observer);
+    }
+
+    removeObserver(_observer: Observer): void {
+        this.observers.splice(this.observers.indexOf(_observer), 1);
+    }
+
+    notifyObservers(): void {
+        for (let observer of this.observers)
+            observer.update(this.nodes.length);
+    }
 
     public createNode(_newNode: T): TreeNode<T> {
         let newNode: TreeNode<T>  = new TreeNode<T>(_newNode);
+        this.nodes.push(newNode);
+        this.notifyObservers();
         return newNode;
+    }
+}
+
+class TreeObserver implements Observer {
+
+    public update(_countNodes: number): void {
+        console.log("Anzahl der Knoten: " + _countNodes);
+    }
+}
+
+function* iAmAGenerator(_tree: Tree<String>): Iterator<String> {
+    for (let i: number = 0; i < _tree.nodes.length; i++) {
+        const inside: String =  _tree.nodes[i].toString();
+        yield inside;
     }
 }
 
 let tree: Tree<String> = new Tree<String>();
 let root: TreeNode<String> = tree.createNode("root");
+
+const iterator: Iterator<String> = iAmAGenerator(tree);
+
+let observer1: TreeObserver = new TreeObserver();
+tree.registerObserver(observer1);
+
 let child1: TreeNode<String> = tree.createNode("child1");
 let child2: TreeNode<String> = tree.createNode("child2");
 let grand11: TreeNode<String> = tree.createNode("grand11");
@@ -72,6 +121,7 @@ let grand13: TreeNode<String> = tree.createNode("grand13");
 let grand21: TreeNode<String> = tree.createNode("grand21");
 let grand22: TreeNode<String> = tree.createNode("grand22");
 let grand212: TreeNode<String> = tree.createNode("grand212");
+
 root.appendChild(child1);
 child1.appendChild(grand11);
 child1.appendChild(grand12);
@@ -81,5 +131,8 @@ root.appendChild(child2);
 grand21.appendChild(grand212);
 child2.appendChild(grand22);
 
-root.printTree();
+console.log(iterator.next());
+
+// root.printTree();
+// console.log(root.search(x => x.includes("212")));
 
