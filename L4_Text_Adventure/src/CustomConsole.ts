45ea738bc3e224a1286fbf17d83bc1a3e57d6c14
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-07-17 18:42:34 +0200

    TASK: build main parts of console, edited concept

--- L4_Text_Adventure/src/CustomConsole.ts
+++ L4_Text_Adventure/src/CustomConsole.ts
@@ -1,8 +1,13 @@
 export class CustomConsole {
     private static instance: CustomConsole; //Ein Singleton macht hier Sinn, da ich nur eine Instanz der Klasse existieren soll
     public siteBody: HTMLBodyElement | null = document.querySelector("#body"); //Musste hier zusätzlich null als Typ mit angeben, weil es auch sein kann, dass kein body existiert
+    public consoleText: HTMLElement | null = document.querySelector("#custom");
+    public consoleInput: HTMLInputElement | null = document.querySelector("#customInput");
 
-    private constructor() {}
+    private constructor() {
+        window.addEventListener("click", this.focusInputField);
+        this.consoleInput?.addEventListener("keydown", this.handleInput);
+    }
 
     public static getInstance(): CustomConsole {
         if (CustomConsole.instance == null)
@@ -12,11 +17,23 @@ export class CustomConsole {
     }
 
     public update(_text: string): void {
-        if (this.siteBody === null)
-            throw new Error("No body found");
-        else {
-            this.siteBody.insertAdjacentHTML("beforeend", "<p>" + _text +  "</p>");
-            this.siteBody.insertAdjacentHTML("beforeend", "<input class=\"custom__input\"></input>");
+        this.focusInputField();
+        this.consoleText!.innerHTML = _text + "<p class=\"custom__commands\">commands(c), look(l), inventory(i), take(t) item, drop(d) item, quit(q)</p>";
+    }
+
+    private handleInput(_event: KeyboardEvent): string {
+        let inputText: string | undefined = "";
+        if (_event.keyCode == 13) {
+            this.consoleInput = document.querySelector("#customInput"); // Muss mir das Input Field immer neu rausziehen, hängt wohl damit zusammen dass eventlisteners asynchron sind
+            inputText = this.consoleInput?.value;
+            if (inputText == undefined)
+                inputText = "";
         }
+        return inputText;
+    }
+
+    private focusInputField(): void {
+        this.consoleInput = document.querySelector("#customInput"); // Muss mir das Input Field immer neu rausziehen, hängt wohl damit zusammen dass eventlisteners asynchron sind
+        this.consoleInput?.focus();
     }
 }
--- L4_Text_Adventure/src/Main.ts
+++ L4_Text_Adventure/src/Main.ts
@@ -4,9 +4,5 @@ namespace TextAdventure {
 
     const customConsole: CustomConsole = CustomConsole.getInstance();
 
-    try {
-        customConsole.update("hello");  
-    } catch (error) {
-        alert(error.message);
-    }
+    customConsole.update("Willkommen zum Textadventure in der griechischen Mythologie. Du schlüpfst in die Rolle von Herakles und musst die ihm auferlegten Aufgaben von Eurystheus auferlegen Aufgaben meistern<br>Wir starten mit der Jagd nach auf den nemeischen Löwen in den lernäischen Wäldern.<br>Du stehst am Waldrand was willst du tun?");  
 }
