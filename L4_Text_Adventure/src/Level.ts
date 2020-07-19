0df1750755e0b78ce813b093265c82b5b5503b38
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-07-19 14:05:55 +0200

    TASK: add getMainCharakterPosition and getFullDescription to concept

--- L4_Text_Adventure/src/Level.ts
+++ L4_Text_Adventure/src/Level.ts
@@ -6,4 +6,24 @@ export class Level {
     constructor(_places: Place[][]) {
         this.places = _places;
     }
+
+    public getMainCharacterPosition(): Place {
+        let horizontalPosition: number = 0;
+        let vertikalPosition: number = 0;
+        for (let i: number = 0; i < this.places.length; i++) {
+            let place: Place[] = this.places[i];
+            for (let j: number = 0; j < place.length; j++) {
+                if (place[j].characters[0] != null) {
+                    for (let character of place[j].characters) {
+                        if (!character.isShown) {
+                            horizontalPosition = i;
+                            vertikalPosition = j;
+                        }
+                    }
+                }
+            }
+        }
+        return this.places[horizontalPosition][vertikalPosition];
+    }
+
 }
