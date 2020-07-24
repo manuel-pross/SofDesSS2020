47820526754e6fe6e2cf58e9ee67a05399325b02
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-07-24 08:32:12 +0200

    TASK: add item-class to concept, add removeItem-method to Place, removed mathods from playableCharacter

--- L4_Text_Adventure/src/PlayableCharacter.ts
+++ L4_Text_Adventure/src/PlayableCharacter.ts
@@ -5,16 +5,4 @@ export class PlayableCharacter extends IntelligentCharacter {
     constructor(_name: string, _selfDescritpion: string, _health: number, _information: string, _inventory: string[], _isShown: boolean) {
         super(_name, _selfDescritpion, _health, _information, _inventory, _isShown);
     }
-
-    public quit(): void {
-        console.log("quit");
-    }
-
-    public commands(): void {
-        console.log("commands");
-    }
-
-    public look(): void {
-        console.log("look");
-    }
 }
