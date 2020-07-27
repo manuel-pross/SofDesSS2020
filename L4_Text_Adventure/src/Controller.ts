fe39f13cda2e755556f0614b480b87dee8ef8624
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-07-27 08:20:39 +0200

    FIX: run build

--- L4_Text_Adventure/src/Controller.ts
+++ L4_Text_Adventure/src/Controller.ts
@@ -132,48 +132,10 @@ export class Controller implements Observer {
                 new Place("Felswand", [], new Inventory([]), true),
                 new Place("Felswand", [], new Inventory([]), false)
             ]
-        ]);
+        ]
+    );
 
-        public update(_userInput: string): void {
-            let placeDescription: string = "";
-            let mainCharacterPlace: Place = this.level1.getCharacterPosition(this.mainCharacter);
-            //let mainCharacterPlaceItems: = this.level1.getCharacterPosition(this.mainCharacter).get
-            if (_userInput == "start" || _userInput == "s") {
-                placeDescription = mainCharacterPlace.getFullDescription();
-                this.customConsole.updateTextfield(placeDescription);
-            } else if (_userInput == "commands" || _userInput == "c") {
-                this.customConsole.updateTextfield("<p class=\"custom__commands\">commands(c), look(l), inventory(i), take(t) item, drop(d) item, quit(q)</p>");
-            } else if (_userInput == "look" || _userInput == "l") {
-                placeDescription = mainCharacterPlace.getFullDescription();
-                this.customConsole.updateTextfield(placeDescription);
-            } else if (_userInput == "inventory" || _userInput == "i") {
-                this.customConsole.updateTextfield(this.mainCharacter.inventory.getInventory());
-            } else if (_userInput.substring(0, 5) == "take " || _userInput.substring(0, 2) == "t ") {
-                let item: Item|null = mainCharacterPlace.items.removeItem(_userInput.substring(_userInput.indexOf(" ") + 1, _userInput.length));
-                if (item != null)
-                    this.mainCharacter.inventory.addItem(item);
-                placeDescription = mainCharacterPlace.getFullDescription();
-                this.customConsole.updateTextfield(placeDescription);
-            } 
-            else if (_userInput == "north" || _userInput == "n" || _userInput == "east" || _userInput == "e" || _userInput == "s" || _userInput == "south" || _userInput == "west" || _userInput == "w") {
-
-                if (_userInput == "n")
-                    _userInput = Direction.North;
-                else if (_userInput == "e")
-                    _userInput = Direction.East;
-                else if (_userInput == "s")
-                    _userInput = Direction.South;
-                else if (_userInput == "w")
-                    _userInput = Direction.West;
-
-                let hasPositionChanged: boolean = this.level1.moveCharacter(this.mainCharacter, _userInput);
-
-                if (hasPositionChanged) {
-                    placeDescription = this.level1.getCharacterPosition(this.mainCharacter).getFullDescription();
-                    this.customConsole.updateTextfield(placeDescription);
-                } else 
-                    this.customConsole.updateTextfield("Dieses Gelände ist unpassierbar. Was möchtest du tun?");
-            }
-        }
+    public update(_userInput: string): void {
+    }
 
 } 
